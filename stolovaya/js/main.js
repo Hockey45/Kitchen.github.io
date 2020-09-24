6478376619218096b43765f017f725207d122a55
Evgeniy <70470204+Hockey45@users.noreply.github.com>
2020-09-25 01:51:15 +0300

    my projects

---
+++ stolovaya/js/main.js
@@ -0,0 +1,18 @@
+$('.works-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    dots:false,
+    startPosition: 1,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
